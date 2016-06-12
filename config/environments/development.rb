a177698ad3c8e32510a129bd4051fc1c293e9102
hieunk58 <khachieunk@gmail.com>
2016-06-12 22:27:13 +0700

    modified host for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,6 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'example.com'
+  host = 'hieunk.herokuapp.com'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 end
