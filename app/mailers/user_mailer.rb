5e8214d95826bf568e6d3fe8dab36ad34c4da951
hieunk58 <khachieunk@gmail.com>
2016-06-12 17:35:15 +0700

    Add password resets & email configuration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,9 +16,9 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    # missing "from: "norply@example.com"" -> error
+    mail to: user.email, from: "noreply@example.com", subject: "Password reset"
   end
 end
