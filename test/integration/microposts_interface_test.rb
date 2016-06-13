0b9bdd5f97eac4806e0a97ed29e4a9085eaa88a1
hieunk58 <khachieunk@gmail.com>
2016-06-13 10:57:50 +0700

    fix missing template destroy micropost

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
 
   def setup
     @user = users(:michael)
